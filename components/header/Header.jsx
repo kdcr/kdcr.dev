3cf39abd19fdc607c3895906f7fc7ff75baa7f27
kdcr <kevincabrerarodriguez@gmail.com>
2022-02-05 10:42:23 +0000

    - Added basic project structure and config - Updated Next.js default files with linting rules - Added new "Header" component

---
+++ components/header/Header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header = () => <div className="w-full h-10 bg-red-500" />;
+
+export default Header;
---
+++ pages/_app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/globals.css';
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+MyApp.propTypes = {
+  Component: PropTypes.node,
+  pageProps: PropTypes.shape({}),
+};
+
+MyApp.defaultProps = {
+  Component: null,
+  pageProps: null,
+};
+
+export default MyApp;
